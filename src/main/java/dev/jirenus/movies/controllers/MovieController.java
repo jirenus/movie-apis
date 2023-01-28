f7c481e0cd211bf8d8ac48ae854ee006c1efc5a0
jirenus781 <ngotuyen781@gmail.com>
2023-01-28 19:53:09 +0700

    feat: /movies/{imdbId} (GET) - get movie by imdbId

--- src/main/java/dev/jirenus/movies/controllers/MovieController.java
+++ src/main/java/dev/jirenus/movies/controllers/MovieController.java
@@ -1,13 +1,17 @@
 package dev.jirenus.movies.controllers;
 
+import dev.jirenus.movies.models.Movie;
 import dev.jirenus.movies.models.ResponseObject;
 import dev.jirenus.movies.services.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/movies")
@@ -24,6 +28,24 @@ public class MovieController {
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(
                     new ResponseObject("failed", "Query movies failed", e.getMessage())
+            );
+        }
+    }
+
+    @GetMapping("/{imdbId}")
+    public ResponseEntity<ResponseObject> getSingleMovie(@PathVariable String imdbId) {
+        try {
+            Optional<Movie> foundMovie = movieService.getSingleMovie(imdbId);
+            return foundMovie.isPresent() ?
+                    ResponseEntity.status(HttpStatus.OK).body(
+                            new ResponseObject("ok", "Query movie successfully", foundMovie)
+                    ) :
+                    ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+                            new ResponseObject("failed", "Cannot find movie with id: " + imdbId, "")
+                    );
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(
+                    new ResponseObject("failed", "Query movie failed", e.getMessage())
             );
         }
     }
--- src/main/java/dev/jirenus/movies/services/MovieService.java
+++ src/main/java/dev/jirenus/movies/services/MovieService.java
@@ -2,10 +2,12 @@ package dev.jirenus.movies.services;
 
 import dev.jirenus.movies.models.Movie;
 import dev.jirenus.movies.repositories.MovieRepository;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
@@ -14,5 +16,9 @@ public class MovieService {
 
     public List<Movie> getAllMovies() {
         return movieRepository.findAll();
+    }
+
+    public Optional<Movie> getSingleMovie(String imdbId) {
+        return movieRepository.findByImdbId(imdbId);
     }
 }
